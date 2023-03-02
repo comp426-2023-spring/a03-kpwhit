a073834a2190f67752da90815bc8d657764f6da6
Koby Whitaker <kpwhit714@gmail.com>
2023-03-02 09:56:19 -0500

    Create bin files

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+const rules = {
+    "rock": ["scissors"],
+    "paper": ["rock"],
+    "scissors": ["paper"]
+}
+
+function rps(player_shot) {
+    let options = Object.keys(rules);
+    let outcome;
+    const opponent_shot = options[Math.floor(Math.random() * options.length)];
+    if (player_shot == opponent_shot) {
+        outcome = 'draw';
+    } else if (rules[player_shot].includes(opponent_shot)) {
+        outcome = 'win';
+    } else {
+        outcome = 'lose';
+    }
+    console.log(outcome);
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+const rules = {
+    "rock": ["scissors", "lizard"],
+    "paper": ["rock", "spock"],
+    "scissors": ["paper", "lizard"],
+    "lizard": ["paper", "spock"],
+    "spock": ["rock", "scissors"]
+
+}
+
+function rps(player_shot) {
+    let options = Object.keys(rules);
+    let outcome;
+    const opponent_shot = options[Math.floor(Math.random() * options.length)];
+    if (player_shot == opponent_shot) {
+        outcome = 'draw';
+    } else if (rules[player_shot].includes(opponent_shot)) {
+        outcome = 'win';
+    } else {
+        outcome = 'lose';
+    }
+    console.log(outcome);
+}
