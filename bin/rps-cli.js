9126a5754a511407983befea5d3a11c2f71a4ab6
Koby Whitaker <kpwhit714@gmail.com>
2023-03-09 17:12:42 -0500

    Complete

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,22 +1,33 @@
 #!/usr/bin/env node
-const rules = {
-    "rock": ["scissors"],
-    "paper": ["rock"],
-    "scissors": ["paper"]
-}
 
-export function rps(player_shot) {
-        console.log(`The player shot is ${player_shot}`);
-    let options = Object.keys(rules);
-    let outcome;
-    const opponent_shot = options[Math.floor(Math.random() * options.length)];
-        console.log(`The opponent shot is ${opponent_shot}`);
-    if (player_shot == opponent_shot) {
-        outcome = 'draw';
-    } else if (rules[player_shot].includes(opponent_shot)) {
-        outcome = 'win';
-    } else {
-        outcome = 'lose';
+import minimist from "minimist";
+import { rps } from "../lib/rpsls\.js";
+
+async function main() {
+    const args = minimist(process.argv.slice(2));
+    if (args.h) {
+        console.log("Usage: node-rps [SHOT]\n");
+        console.log("Play Rock Paper Scissors (RPS)");
+        console.log("");
+        console.log("  -h, --help      display this help message and exit");
+        console.log("  -r, --rules     display the rules and exit");
+        console.log("")
+        console.log("Examples:")
+        console.log("  node-rps        Return JSON with single player RPS result.")
+        console.log(`                  e.g. {"player":"rock"}`)
+        console.log("  node-rps rock   Return JSON with results for RPS played against a simulated opponent.")
+        console.log(`                  e.g {"player":"rock","opponent":"scissors","result":"win"}`)
+        return 0;
     }
-    return outcome;
-}
+    if (args.r) {
+        console.log("Rules for Rock Paper Scissors:");
+        console.log("");
+        console.log("  - Scissors CUTS Paper");
+        console.log("  - Paper COVERS Rock");
+        console.log("  - Rock CRUSHES Scissors");
+        return 0;
+    }
+    console.log(rps(args._[0]));
+};
+
+main();
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,23 +1,39 @@
 #!/usr/bin/env node
-const rules = {
-    "rock": ["scissors", "lizard"],
-    "paper": ["rock", "spock"],
-    "scissors": ["paper", "lizard"],
-    "lizard": ["paper", "spock"],
-    "spock": ["rock", "scissors"]
+import minimist from "minimist";
+import { rpsls } from "../lib/rpsls\.js";
 
-}
-
-export function rpsls(player_shot) {
-    let options = Object.keys(rules);
-    let outcome;
-    const opponent_shot = options[Math.floor(Math.random() * options.length)];
-    if (player_shot == opponent_shot) {
-        outcome = 'draw';
-    } else if (rules[player_shot].includes(opponent_shot)) {
-        outcome = 'win';
-    } else {
-        outcome = 'lose';
+async function main() {
+    const args = minimist(process.argv.slice(2));
+    if (args.h) {
+        console.log("Usage: node-rpsls [SHOT]\n");
+        console.log("Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!");
+        console.log("");
+        console.log("  -h, --help      display this help message and exit");
+        console.log("  -r, --rules     display the rules and exit");
+        console.log("")
+        console.log("Examples:")
+        console.log("  node-rpsls        Return JSON with single player RPSLS result.")
+        console.log(`                  e.g. {"player":"rock"}`)
+        console.log("  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.")
+        console.log(`                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`)
+        return 0;
     }
-    return outcome;
+    if (args.r) {
+        console.log("Rules for the Lizard-Spock Espansion of Rock Paper Scissors:");
+        console.log("");
+        console.log("  - Scissors CUTS Paper");
+        console.log("  - Paper COVERS Rock");
+        console.log("  - Rock SMOOSHES Lizard");
+        console.log("  - Lizard POISONS Spock");
+        console.log("  - Spock SMASHES Scissors");
+        console.log("  - Scissors DECAPITATES Lizard");
+        console.log("  - Lizard EATS Paper");
+        console.log("  - Paper DISPROVES Spock");
+        console.log("  - Spock VAPORIZES Rock");
+        console.log("  - Rock CRUSHES Scissors");
+        return 0;
+    }
+    console.log(rpsls(args._[0]));
 }
+
+main();
